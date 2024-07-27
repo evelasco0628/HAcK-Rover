fc9b15af507eaf8606075be2c594bbf520b26b8c
Enrique Velasco <evelasco0628@gmail.com>
2024-07-27 12:05:18 -0700

    Updated pico to allow wheels to move from frontend

--- frontend/src/pages/Control.js
+++ frontend/src/pages/Control.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from 'socket.io-client';
+import './Control.css';
 
 const socket = io('http://localhost:8000');
 
@@ -7,6 +8,7 @@ function Control() {
   const [temp, setTemp] = useState(null);
   const [ultrasonic, setUltrasonic] = useState(null);
   const [humidity, setHumidity] = useState(null);
+  const [textColor, setTextColor] = useState('black');
 
   useEffect(() => {
     // Listen for temperature updates
@@ -30,6 +32,35 @@ function Control() {
     };
   }, []);
 
+  useEffect( () => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'w') {
+        setTextColor('blue');
+        sendDirection('forward');
+      } else if (event.key === 'a') {
+        setTextColor('red')
+        sendDirection('left');
+      } else if (event.key === 's') {
+        sendDirection('backward');
+      } else if (event.key === 'd') {
+        sendDirection('right');
+      }
+    }
+
+    const handleKeyUp = () => {
+      setTextColor('black')
+      sendDirection('stop');
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [])
+
   const sendDirection = (direction) => {
     socket.emit('send-direction', direction);
   };
@@ -43,7 +74,7 @@ function Control() {
       <h1>Control</h1>
       <button onClick={() => sendDirection('left')}>Left</button>
       <button onClick={() => sendDirection('right')}>Right</button>
-      <button onClick={() => sendDirection('forward')}>Forward</button>
+      <button onClick={() => sendDirection('forward')}>Forward</button> 
       <button onClick={() => sendDirection('backward')}>Backward</button>
     </div>
   );
--- pico/examples.py
+++ pico/examples.py
@@ -1,31 +1,78 @@
 from connections import connect_mqtt, connect_internet
 from time import sleep
-from constants import ssid, mqtt_server, mqtt_user, mqtt_pass
+from machine import Pin
+from constants import ssid, password, mqtt_server, mqtt_user, mqtt_pass
 
 
 # Function to handle an incoming message
 
+in1 = Pin(15, Pin.OUT) 
+in2 = Pin(14, Pin.OUT)
+in3 = Pin(16, Pin.OUT)
+in4 = Pin(17, Pin.OUT)
+
 def cb(topic, msg):
-#     print(f"Topic: {topic}, Message: {msg}")
-    if topic == b"mytopic":
-        print("Message from mytopic")
-    
+    print(f"Topic: {topic}, Message: {msg}")
+    if topic == b"direction":
+        if msg == b"forward":
+            print("Move forward")
+            in1.value(1)
+            in2.value(0)
+            in3.value(1)
+            in4.value(0)
+            
+        elif msg == b"backward":
+            print("Move backward")
+            in1.value(0)
+            in2.value(1)
+            in3.value(0)
+            in4.value(1)
+            
+        elif msg == b"right":
+            print("Move right")
+            in1.value(0)
+            in2.value(0)
+            in3.value(0)
+            in4.value(1)
+            
+        elif msg == b"left":
+            print("Move left")
+            in1.value(0)
+            in2.value(1)
+            in3.value(0)
+            in4.value(0)
+            
+        elif msg == b"stop":
+            print("stop")
+            in1.value(0)
+            in2.value(0)
+            in3.value(0)
+            in4.value(0)
+        
 
 def main():
     try:
-        connect_internet(ssid,password=None)
+        connect_internet(ssid,password)
         client = connect_mqtt(mqtt_server, mqtt_user, mqtt_pass)
 
         client.set_callback(cb)
-        client.subscribe("mytopic")
-        client.publish("mytopic", "message")
+        client.subscribe("direction")
+        client.publish("temp", "89")
         while True:
             client.check_msg()
             sleep(1)
-
+            
+            
     except KeyboardInterrupt:
         print('keyboard interrupt')
         
+
+    finally:
+        in1.value(0)
+        in2.value(0)
+        in3.value(0)
+        in4.value(0)
         
 if __name__ == "__main__":
     main()
+
